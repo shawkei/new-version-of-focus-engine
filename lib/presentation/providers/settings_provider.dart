04bf2c7d315a0a2d209e90c96a3b031911213947
shawkei <113327758+shawkei@users.noreply.github.com>
2026-04-24 21:47:21 +0300

    Refactor SettingsNotifier and settingsProvider
    Refactor SettingsNotifier to remove unused methods and simplify state management.

--- lib/presentation/providers/settings_provider.dart
+++ lib/presentation/providers/settings_provider.dart
@@ -1,41 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/models/session.dart';
 import '../../data/models/user_settings.dart';
 import '../../data/repositories/session_repository.dart';
-
-class SettingsNotifier extends StateNotifier<UserSettings> {
-  final SessionRepository _repository;
-
-  SettingsNotifier(this._repository) : super(const UserSettings()) {
-    _loadSettings();
-  }
-
-  Future<void> _loadSettings() async {
-    final settings = await _repository.getSettings();
-    state = settings;
-  }
-
-  Future<void> setEnergy(EnergyLevel energy) async {
-    state = state.copyWith(energyLevel: energy);
-    await _repository.saveSettings(state);
-  }
-
-  Future<void> setGoal(FocusGoal goal) async {
-    state = state.copyWith(defaultGoal: goal);
-    await _repository.saveSettings(state);
-  }
-
-  Future<void> setDailyGoal(int goal) async {
-    state = state.copyWith(dailyGoal: goal);
-    await _repository.saveSettings(state);
-  }
-
-  Future<void> setInsight(String insight) async {
-    state = state.copyWith(lastInsight: insight);
-    await _repository.saveSettings(state);
-  }
-}
 
 final settingsProvider = StateNotifierProvider<SettingsNotifier, UserSettings>((ref) {
   return SettingsNotifier(ref.watch(sessionRepositoryProvider));
 });
+
+class SettingsNotifier extends StateNotifier<UserSettings> {
+  final SessionRepository _repository;
+  
+  SettingsNotifier(this._repository) : super(const UserSettings());
+  
+  void setEnergy(EnergyLevel energy) {
+    state = state.copyWith(energyLevel: energy);
+    _repository.saveSettings(state);
+  }
+  
+  void updateStreak(int streak) {
+    state = state.copyWith(weeklyStreak: streak);
+    _repository.saveSettings(state);
+  }
+}
