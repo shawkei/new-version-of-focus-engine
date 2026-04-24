7367dfa7809e8c7090297d997d1922f09d5c1273
shawkei <113327758+shawkei@users.noreply.github.com>
2026-04-24 22:09:10 +0300

    Add FocusGoal enum and defaultGoal to UserSettings

--- lib/data/models/user_settings.dart
+++ lib/data/models/user_settings.dart
@@ -1,16 +1,20 @@
 import 'energy_level.dart';
 
+enum FocusGoal { study, work, skill, custom }
+
 class UserSettings {
   final int weeklyStreak;
   final EnergyLevel energyLevel;
   final String? lastInsight;
   final int dailyGoal;
+  final FocusGoal defaultGoal;
   
   const UserSettings({
     this.weeklyStreak = 0,
     this.energyLevel = EnergyLevel.normal,
     this.lastInsight,
     this.dailyGoal = 4,
+    this.defaultGoal = FocusGoal.focus,
   });
   
   UserSettings copyWith({
@@ -18,12 +22,14 @@ class UserSettings {
     EnergyLevel? energyLevel,
     String? lastInsight,
     int? dailyGoal,
+    FocusGoal? defaultGoal,
   }) {
     return UserSettings(
       weeklyStreak: weeklyStreak ?? this.weeklyStreak,
       energyLevel: energyLevel ?? this.energyLevel,
       lastInsight: lastInsight ?? this.lastInsight,
       dailyGoal: dailyGoal ?? this.dailyGoal,
+      defaultGoal: defaultGoal ?? this.defaultGoal,
     );
   }
 }
