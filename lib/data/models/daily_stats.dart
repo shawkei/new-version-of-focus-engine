f8639c91c5d318d331da84bd970c53f3b375ef18
shawkei <113327758+shawkei@users.noreply.github.com>
2026-04-24 22:00:15 +0300

    Add weeklySessions field to DailyStats model

--- lib/data/models/daily_stats.dart
+++ lib/data/models/daily_stats.dart
@@ -3,12 +3,14 @@ class DailyStats {
   final int dailyGoal;
   final int weeklyTotal;
   final int weeklyStreak;
+  final int weeklySessions;
   
   const DailyStats({
     this.todayCompleted = 0,
     this.dailyGoal = 4,
     this.weeklyTotal = 0,
     this.weeklyStreak = 0,
+    this.weeklySessions = 0,
   });
   
   DailyStats copyWith({
@@ -16,12 +18,14 @@ class DailyStats {
     int? dailyGoal,
     int? weeklyTotal,
     int? weeklyStreak,
+    int? weeklySessions,
   }) {
     return DailyStats(
       todayCompleted: todayCompleted ?? this.todayCompleted,
       dailyGoal: dailyGoal ?? this.dailyGoal,
       weeklyTotal: weeklyTotal ?? this.weeklyTotal,
       weeklyStreak: weeklyStreak ?? this.weeklyStreak,
+      weeklySessions: weeklySessions ?? this.weeklySessions,
     );
   }
 }
